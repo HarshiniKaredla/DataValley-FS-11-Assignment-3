f155d649a7f0815d73c4007e1fe2594465fffcdf
HarshiniKaredla <117500919+HarshiniKaredla@users.noreply.github.com>
2024-03-18 22:14:13 +0530

    Serialization

--- serialization.java
+++ serialization.java
@@ -1,3 +1,10 @@
+To implement serialization in Java for a given object, such as the 'Customer' class in your example, you need to follow these steps:
+
+Ensure that the class you want to serialize implements the Serializable interface.
+Create an instance of the ObjectOutputStream class to write objects to an output stream.
+Use the writeObject() method of ObjectOutputStream to serialize the object and write it to a file.
+Close the output stream to release resources.
+    
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
